54d584a551dd7e95b97fabcd695b857ffe98b68c
chravindra414 <chravindra414@gmail.com>
2020-04-10 19:16:42 -0500

    CHanges for deployemnet

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*s*uz4x8u&@_u4fmvnt2ou2!6(4mfg7xqw36676u72nrdtth3q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
